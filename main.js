07eeb5a5b7710bebe0a2ebfa8280d4270ea2777f
Dloft <derek.lofton1@gmail.com>
2019-07-21 15:46:38 -0400

    js

---
+++ main.js
@@ -0,0 +1,18 @@
+
+function moveUp() {
+    alert("This is up");
+}
+
+function moveDown() {
+    alert("This is down");
+}
+
+function moveLeft() {
+    alert("This is left");
+}
+
+function moveRight() {
+    alert("This is right");
+}
+
+
