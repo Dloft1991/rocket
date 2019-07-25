d6487a222065db2ad1d7bdab7dd1fb514d7f913f
Dloft <derek.lofton1@gmail.com>
2019-07-25 18:05:12 -0400

    rocket

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
+function Rocket() {
+//    const rocket = document.getElementById("rocket");
+   this.x = 0;
+   this.y = 0;
+   this.xspeed = 0;
+   this.yspeed = 0;
+}
+
 
 function moveUp() {
-    alert("This is up");
+    console.log("This is up");
+    rocket.posY++;
 }
 
 function moveDown() {
-    alert("This is down");
+    console.log("This is down");
+    rocket.posY--;
 }
 
 function moveLeft() {
-    alert("This is left");
+    console.log("This is left");
 }
 
 function moveRight() {
-    alert("This is right");
+    console.log("This is right");
 }
-
-
