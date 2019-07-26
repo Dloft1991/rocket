6760f1c4379a8f89ff1610dccc344167ab9e43e1
Dloft <derek.lofton1@gmail.com>
2019-07-26 19:15:21 -0400

    aliens

--- main.js
+++ main.js
@@ -1,26 +1,33 @@
 var rocket=document.getElementById("rocket");
+var alien=document.getElementById("alien");
 var container=document.getElementById("container");
 
 var rocketLeft = 550;
 var rocketTop = 500;
 
+var alienLeft = 0;
+var alienTop = 50;
+
 function anim(e){
     if(e.keyCode==39){
-        rocketLeft +=15;
-        rocket.style.left = rocketLeft + 'px';
+        alienLeft +=15;
+        alien.style.left = alienLeft + 'px';
         console.log("left")
     }
     if(e.keyCode==37){
-        rocketLeft -=15;
-        rocket.style.left = rocketLeft + 'px';
-        if(rocketLeft <=0){
-            rocketLeft +15;
+        alienLeft -=15;
+        alien.style.left = alienLeft + 'px';
+        if(alienLeft <=0){
+            alienLeft +15;
+        }
+        else if(alienLeft ==100) {
+            alienLeft = 0;
         }
     }
-    if(e.keyCode==40){
-        rocketTop +=15;
-        rocket.style.top = rocketTop + 'px';
-    }
+    // if(e.keyCode==40){
+    //     rocketTop +=15;
+    //     rocket.style.top = rocketTop + 'px';
+    // }
     if(e.keyCode==38){
         rocketTop -=15;
         rocket.style.top = rocketTop + 'px';
@@ -28,8 +35,9 @@ function anim(e){
             rocketTop +15;
             console.log("up")
         }
-        else if(rocketTop ===50) {
-            rocketTop -450;
+        else if(rocketTop <=100) {
+            rocketTop = 515;
+            rocketLeft = 550;
         }
     }
 }
